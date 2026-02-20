9fc43083f36b9b6632cdc7886013c511074d634a
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2026-02-21 00:45:11 +0530

    add: main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
 
 model = OllamaLLM(model= "llama3.2")
 
@@ -19,8 +20,10 @@ while True:
     print("Pizza bot response: ")
     if question == "q":
         break
+
+    reviews = retriever.invoke(question)
     result = chain.invoke({
-    "reviews": [],
+    "reviews": reviews,
     "question": question
     })
     print(result)
