e2417025e19fa0f99226545e03d3c47a09eadd90
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2026-02-20 22:42:16 +0530

    data: realistic restaurant reviesw data added

---
+++ vector.py
@@ -0,0 +1,9 @@
+# from langchain_ollama import OllamaEmbeddings
+# from langchain_chroma import Chroma
+# from langchain_core.documents import Document
+# import os
+# import pandas as pd
+
+# df = pd.read_csv("data/realistic_restaurant_reviews.csv")
+import os
+print(os.path.getsize("data/realistic_restaurant_reviews.csv"))
